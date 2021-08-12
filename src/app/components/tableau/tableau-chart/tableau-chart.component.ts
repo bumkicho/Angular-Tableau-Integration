1bee2cd9304d583ab3cbf24f8db382c1eb3d44a1
ersimont <8042088+ersimont@users.noreply.github.com>
2021-08-12 10:46:58 -0400

    small formatting changes, remove unused script & div

--- src/app/components/tableau/tableau-chart/tableau-chart.component.ts
+++ src/app/components/tableau/tableau-chart/tableau-chart.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { TableauService } from 'src/app/services/tableau.service';
-import { TableauChart } from 'src/TableauChart';
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
+import { TableauService } from "src/app/services/tableau.service";
+import { TableauChart } from "src/TableauChart";
 
 @Component({
-  selector: 'app-tableau-chart',
-  templateUrl: './tableau-chart.component.html',
-  styleUrls: ['./tableau-chart.component.css']
+  selector: "app-tableau-chart",
+  templateUrl: "./tableau-chart.component.html",
+  styleUrls: ["./tableau-chart.component.css"],
 })
 export class TableauChartComponent implements OnInit {
-  @Input() chart: TableauChart;
+  @Input() private chart: TableauChart;
 
   constructor(tableauService: TableauService) {
     // should I need to do something with tableauService to refresh tableau chart on the page?
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
